098d1897154584b47e65aeb4fa71ff69aeddae7f
AZAZAR1 <anthony.azar@cdc-advisory.com>
2026-02-23 20:30:00 +0400

    Create system.js

---
+++ pages/system.js
@@ -0,0 +1,19 @@
+import Layout from "../components/Layout";
+
+export default function System() {
+  return (
+    <Layout>
+      <div className="section">
+        <div className="container">
+          <h2>Scientific Peak-Flavor System™</h2>
+          <div className="card" style={{ marginTop: 14 }}>
+            <p className="small">
+              This is a public-safe overview of the CSI framework. CSI does not publish proprietary calibration formulas on the public site.
+              The system overview communicates principles and outcomes while preserving institutional IP.
+            </p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
