eeecef94866d674a386d3e3ad291658cba849ab1
AZAZAR1 <anthony.azar@cdc-advisory.com>
2026-02-24 20:07:25 +0400

    Update programs.js

--- pages/programs.js
+++ pages/programs.js
@@ -5,8 +5,8 @@ import { COPY } from "../components/copy";
 
 export default function Programs() {
   const { locale } = useRouter();
-  const lang = locale || "en";
-  const c = COPY[lang];
+  const lang = (locale || "en").toLowerCase();
+  const c = COPY[lang] || COPY.en;
 
   return (
     <Layout>
@@ -16,32 +16,82 @@ export default function Programs() {
           <p className="small">{c.programs_lead}</p>
 
           <div className="grid3" style={{ marginTop: 14 }}>
-            <div className="card">
-              <h3>{c.ccs_title}</h3>
-              <p>{c.ccs_blurb}</p>
-              <div className="ctaRow">
-                <Link className="btn primary" href="/ccs" locale={lang}>{c.apply}</Link>
-                <a className="btn" href="/brochures/CCS_Certified_Cigar_Sommelier_Brochure.pdf">{c.download}</a>
+
+            {/* CCS */}
+            <div className="card programCard">
+              <div className="programBody">
+                <h3>{c.ccs_title}</h3>
+                <p>{c.ccs_blurb}</p>
+              </div>
+
+              <div className="ctaRow programButtons">
+                <Link
+                  className="btn primary"
+                  href={{ pathname: "/contact", query: { program: "ccs" } }}
+                  locale={lang}
+                >
+                  {c.apply}
+                </Link>
+
+                <a
+                  className="btn"
+                  href="/brochures/CCS_Certified_Cigar_Sommelier_Brochure.pdf"
+                >
+                  {c.download}
+                </a>
               </div>
             </div>
 
-            <div className="card">
-              <h3>{c.acs_title}</h3>
-              <p>{c.acs_blurb}</p>
-              <div className="ctaRow">
-                <Link className="btn primary" href="/acs" locale={lang}>{c.apply}</Link>
-                <a className="btn" href="/brochures/ACS_Advanced_Cigar_Sommelier_Brochure.pdf">{c.download}</a>
+            {/* ACS */}
+            <div className="card programCard">
+              <div className="programBody">
+                <h3>{c.acs_title}</h3>
+                <p>{c.acs_blurb}</p>
+              </div>
+
+              <div className="ctaRow programButtons">
+                <Link
+                  className="btn primary"
+                  href={{ pathname: "/contact", query: { program: "acs" } }}
+                  locale={lang}
+                >
+                  {c.apply}
+                </Link>
+
+                <a
+                  className="btn"
+                  href="/brochures/ACS_Advanced_Cigar_Sommelier_Brochure.pdf"
+                >
+                  {c.download}
+                </a>
               </div>
             </div>
 
-            <div className="card">
-              <h3>{c.amc_title}</h3>
-              <p>{c.amc_blurb}</p>
-              <div className="ctaRow">
-                <Link className="btn primary" href="/amc" locale={lang}>{c.request_invite}</Link>
-                <a className="btn" href="/brochures/AMC_Aficionado_Master_Class_Brochure_v2.pdf">{c.download}</a>
+            {/* AMC */}
+            <div className="card programCard">
+              <div className="programBody">
+                <h3>{c.amc_title}</h3>
+                <p>{c.amc_blurb}</p>
+              </div>
+
+              <div className="ctaRow programButtons">
+                <Link
+                  className="btn primary"
+                  href={{ pathname: "/contact", query: { program: "amc" } }}
+                  locale={lang}
+                >
+                  {c.request_invite}
+                </Link>
+
+                <a
+                  className="btn"
+                  href="/brochures/AMC_Aficionado_Master_Class_Brochure_v2.pdf"
+                >
+                  {c.download}
+                </a>
               </div>
             </div>
+
           </div>
         </div>
       </div>
