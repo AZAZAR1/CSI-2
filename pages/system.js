d80c710b7027bce3329100b306e516dda389c624
AZAZAR1 <anthony.azar@cdc-advisory.com>
2026-02-24 13:52:22 +0400

    Update system.js

--- pages/system.js
+++ pages/system.js
@@ -4,15 +4,13 @@ export default function System() {
   return (
     <Layout>
       <div className="heroCard" style={{ marginTop: 14 }}>
-  <div className="heroFrame">
-    <img src="/img/peak-flavor-system.png" alt="Scientific Peak-Flavor System" />
-  </div>
-  <div className="heroCardPad">
-    <div className="heroTag"><i>Scientific Peak-Flavor System™</i></div>
-  </div>
-</div>
-        
-
+        <div className="heroFrame">
+          <img src="/img/peak-flavor-system.png" alt="Scientific Peak-Flavor System" />
+        </div>
+        <div className="heroCardPad">
+          <div className="heroTag"><i>Scientific Peak-Flavor System™</i></div>
+        </div>
+     </div>
     </Layout>
   );
 }
