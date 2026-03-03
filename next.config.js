2d8441ab1d69e5eb76081431e671148ebb074e7e
AZAZAR1 <anthony.azar@cdc-advisory.com>
2026-03-04 00:23:28 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,26 @@ const nextConfig = {
   i18n: {
     locales: ["en", "fr", "de"],
     defaultLocale: "en"
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/programs",
+        destination: "/courses",
+        permanent: true
+      },
+      {
+        source: "/fr/programs",
+        destination: "/fr/courses",
+        permanent: true
+      },
+      {
+        source: "/de/programs",
+        destination: "/de/courses",
+        permanent: true
+      }
+    ];
   }
 };
 
