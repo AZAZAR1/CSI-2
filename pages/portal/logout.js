618270d01edde0370243f324e9bfadf5cc048989
AZAZAR1 <anthony.azar@cdc-advisory.com>
2026-03-05 18:22:50 +0400

    Delete logout.js

--- pages/portal/logout.js
+++
@@ -1,5 +0,0 @@
-// pages/api/portal/logout.js
-export default function handler(req, res) {
-  res.setHeader("Set-Cookie", "portal_session=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax");
-  return res.status(200).json({ ok: true });
-}
