204b0455536f1f56ee9d6ac606613bd04d371b33
AZAZAR1 <anthony.azar@cdc-advisory.com>
2026-09-07 14:12:12 +0400

    Update access key to use CPFS key

--- pages/cpfs-implementation.js
+++ pages/cpfs-implementation.js
@@ -145,7 +145,7 @@ export default function CPFSImplementation() {
                     <input
                       type="hidden"
                       name="accessKey"
-                      value={process.env.NEXT_PUBLIC_STATICFORMS_ACCESS_KEY || ""}
+                      value={process.env.NEXT_PUBLIC_STATICFORMS_CPFS_KEY || ""}
                     />
                     <input type="hidden" name="redirectTo" value={redirectTo} />
                     <input
