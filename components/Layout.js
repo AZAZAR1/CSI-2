6b96a13d72d382fa15393ac64647fd69e8eccb15
AZAZAR1 <anthony.azar@cdc-advisory.com>
2026-09-09 18:34:17 +0400

    Add topBanner prop to Layout component
    Added optional topBanner prop to Layout component.

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,10 @@
 import Nav from "./Nav";
 import Footer from "./Footer";
 
-export default function Layout({ children }) {
+export default function Layout({ children, topBanner = null }) {
   return (
     <>
+      {topBanner}
       <Nav />
       {children}
       <Footer />
